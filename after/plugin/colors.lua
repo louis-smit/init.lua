4d6b0c73f706c380acdf8ca32e75f0a95abf0dcf
Louis Smit <louissm.it@icloud.com>
2023-06-02 03:05:34 +0200

    first commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,10 @@
+-- Small tweak for Rose Pine
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) 
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) 
+end
+
+ColorMyPencils()
